fdf88d45c78d0612ff603ab4cd4d33a848694147
gary909 <garywhite61@hotmail.com>
2023-03-20 13:08:05 +0100

    code working

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-function all( arr, fun ){
-    // ...
-}
+function all(arr, fun){
+    if (arr == undefined || arr == []) {
+      return true;
+    }
+    for (let i = 0; i < arr.length; i ++) {
+      if (fun(arr[i]) == false) {
+        return false;
+      }
+    }
+    return true;
+  }
 
 console.log(all([1,2,3,4,5], function(v){return v<9})); // true
 console.log(all([1,2,3,4,5], function(v){return v>9})); // false
